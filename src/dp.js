06d4096bdd961547cfb249b17406914a6da2763d
Nima <nimaonsafari@gmail.com>
2012-03-17 16:03:56 +0100

    added option for starting on mondays or sundays

--- src/dp.js
+++ src/dp.js
@@ -1,10 +1,8 @@
 var dp = (function() {
   
-  var now;
-  var firstOfMonth;
   var d = document;
-  
-  var fullWeek = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+  var fullWeek = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
   var fullYear = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
   
   function toggleDate(year, month) {
@@ -12,38 +10,41 @@ var dp = (function() {
 	createTable(year, month);
   }
   
-  function createTable(year, month) {
-  	console.time('createTable');
-  	now = new Date(year, month, 0);
-  	firstOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+  function createTable(year, month, sunStart) {
+  	var now = new Date(year, month, 0);
+  	var firstOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
   	
     var table = d.createElement("table");
     var thead = d.createElement("thead");
     var tr = d.createElement("tr");
     var tcell = d.createElement("th");
     
-    var prevMonthArrow = d.createElement("a");
-    prevMonthArrow.addEventListener('click', function() { toggleDate(year, month - 1) }, false);
-    prevMonthArrow.setAttribute("href", "#");
-    prevMonthArrow.appendChild(d.createTextNode("<"));
-    prevMonthArrow.style.float = "left";
+    var link = d.createElement("a");
+    link.addEventListener('click', function() { toggleDate(year, month - 1) }, false);
+    link.setAttribute("href", "#");
+    link.appendChild(d.createTextNode("<"));
+    link.style['float'] = "left";
+    tcell.appendChild(link);
     
-    var nextMonthArrow = d.createElement("a");
-    nextMonthArrow.addEventListener('click', function() { toggleDate(year, month + 1) }, false);
-    nextMonthArrow.setAttribute("href", "#");
-    nextMonthArrow.appendChild(d.createTextNode(">"));
-    nextMonthArrow.style.float = "right";
-    
-    tcell.appendChild(prevMonthArrow);
     tcell.appendChild(d.createTextNode(fullYear[now.getMonth()] + " " + now.getFullYear()));
-    tcell.appendChild(nextMonthArrow);
+    
+    link = d.createElement("a");
+    link.addEventListener('click', function() { toggleDate(year, month + 1) }, false);
+    link.setAttribute("href", "#");
+    link.appendChild(d.createTextNode(">"));
+    link.style['float'] = "right";
+    tcell.appendChild(link);
     
     tcell.setAttribute("colspan", "7");
     tcell.setAttribute("align", "center");
 	tr.appendChild(tcell);
     thead.appendChild(tr);
     tr = d.createElement("tr");
-    for(var i = 0; i < fullWeek.length; i++) {
+	
+	var weekStart = sunStart ? 0 : 1;
+	var weekEnd = sunStart ? fullWeek.length - 1 : fullWeek.length;
+	
+    for(i = weekStart; i < weekEnd; i++) {
 	  tcell = d.createElement("th");
 	  tcell.appendChild(d.createTextNode(fullWeek[i]));
 	  tr.appendChild(tcell);
@@ -54,12 +55,25 @@ var dp = (function() {
 	var row = d.createElement("tr");
 	var tdsInRow = 1;
 	var dayOffset = firstOfMonth.getDay();
+	
 	var daysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+		
+	if(!sunStart && dayOffset === 0) {
+		dayOffset = 7;	
+	}
+	
+	for(var i = weekStart; i < dayOffset; i++) {
+		tcell = d.createElement("td");
+		row.appendChild(tcell);
+		tdsInRow++;
+	}
 
-    for(var i = 1 - dayOffset; i <= daysInMonth; i++) {
-        tcell = d.createElement("td");        
-        var cellText = d.createTextNode(i > 0 ? i : "");
-        tcell.appendChild(cellText);
+    for(var i = 1; i <= daysInMonth; i++) {
+        tcell = d.createElement("td");    
+        link = d.createElement("a");
+        link.setAttribute('href', '#');
+        link.appendChild(d.createTextNode(i));
+        tcell.appendChild(link);
         row.appendChild(tcell);
         if(tdsInRow % 7 === 0) {
         	tableBody.appendChild(row);
@@ -73,8 +87,6 @@ var dp = (function() {
     table.appendChild(tableBody);
     table.setAttribute('id', 'cal');
     d.body.appendChild(table);
-    
-    console.timeEnd('createTable');
   }
 
   return {
